8053bc45de72eb7968034162add7c9a13361850c
molitomoki <molitomoki@gmail.com>
2023-04-06 16:07:49 +0900

    FactoryBotを導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe "ユーザー新規登録" do
     it "nameが空だと登録できない" do
-      user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Name can't be blank")
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
     end
     it "emailが空では登録できない" do
-      user = User.new(name: "abe", email: "", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空だと登録できない' do
-      user = User.new(name: "abe", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Password can't be blank")
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'password_confirmationがpasswordと一致しないと登録できない' do
-      user = User.new(name: "abe", email: "kkk@gmail.com", password: "00000000", password_confirmation: "")
-      user.valid? 
-      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      @user.password_confirmation = ''
+      @user.valid? 
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
   end
 end
