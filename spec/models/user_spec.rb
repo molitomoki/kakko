7d282784ad4d96c931fdf75f7cc827c038bd1f84
molitomoki <molitomoki@gmail.com>
2023-04-06 17:23:43 +0900

    Userモデルの単体テストを追加（Fakerを導入）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,25 +6,61 @@ RSpec.describe User, type: :model do
   end
 
   describe "ユーザー新規登録" do
-    it "nameが空だと登録できない" do
-      @user.name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Name can't be blank")
-    end
-    it "emailが空では登録できない" do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'passwordが空だと登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'password_confirmationがpasswordと一致しないと登録できない' do
-      @user.password_confirmation = ''
-      @user.valid? 
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    context '新規登録できるとき' do
+      it 'nameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
       end
+    end
+    context '新規登録できないとき' do
+      it "nameが空だと登録できない" do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid? 
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'nameが7文字以上では登録できない' do
+        @user.name = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Name is too long (maximum is 6 characters)')
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+      end
+    end
   end
 end
