a90f6aaa8662ae74c791135d4ba5898f04f20cec
molitomoki <molitomoki@gmail.com>
2023-04-07 12:25:55 +0900

    投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
