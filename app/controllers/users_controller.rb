ee069a5d3818106e24d5c05e45c4c0648d5721f1
molitomoki <molitomoki@gmail.com>
2023-04-12 18:22:27 +0900

    いいねの一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
   end
 end
