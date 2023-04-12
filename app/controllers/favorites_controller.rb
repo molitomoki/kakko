d69e7dbce09ec77acb1da340db03c8c89e88ecbc
molitomoki <molitomoki@gmail.com>
2023-04-12 17:11:53 +0900

    非同期処理の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
+
   def create
     favorite = current_user.favorites.build(post_id: params[:post_id])
     favorite.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
